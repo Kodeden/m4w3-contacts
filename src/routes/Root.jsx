c3011df60446697301f753533cc5c36c4e612c95
Manav Misra <manavm@visionwebsoft.com>
2022-12-26 13:05:50 -0600

    :truck: Users to Root
    Includes additon of heading

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,7 +1,7 @@
 import { useLoaderData } from "react-router-dom";
 import Table from "../components/Table/Table";
 
-export default function Users() {
+export default function Root() {
   // useLoaderData() is a hook that returns the data - be sure to DESTRUCTURE it!
   const { users } = useLoaderData();
 
@@ -11,5 +11,12 @@ export default function Users() {
     username,
   }));
 
-  return <Table headers={Object.keys(usersData[0])} data={usersData} />;
+  return (
+    <>
+      <h1 className="mt-4 mb-12 text-center text-3xl font-bold underline">
+        Contacts
+      </h1>
+      <Table headers={Object.keys(usersData[0])} data={usersData} />
+    </>
+  );
 }
