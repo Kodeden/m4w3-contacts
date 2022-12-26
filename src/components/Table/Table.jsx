19d849fce8e8acc074ded896dd17043370ac037b
Manav Misra <manavm@visionwebsoft.com>
2022-12-26 17:57:44 -0600

    feat: :sparkles: next/prev data pagination

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,15 +1,44 @@
+import usePagination from "@/hooks/usePagination";
 import PropTypes from "prop-types";
 import TBody from "./TBody";
 import TH from "./TH";
-import usePagination from "@/hooks/usePagination";
 
 export default function Table({ headers, data }) {
-  const { currentData } = usePagination(data);
+  const { currentData, currentPage, maxPage, dispatchPagination } =
+    usePagination(data);
+
+  const isFirstPage = currentPage === 1;
+  const isLastPage = currentPage === maxPage;
 
   return (
     <table className="container mx-auto max-w-max table-auto">
       <TH headers={headers} />
       <TBody data={currentData} />
+      <tfoot>
+        <tr>
+          <td colSpan={headers.length} className="text-center [&>*]:mx-4">
+            <button
+              className={isFirstPage ? "text-gray-500" : "text-blue-500"}
+              disabled={isFirstPage}
+              onClick={() => dispatchPagination({ type: "PREV" })}
+            >
+              Previous
+            </button>
+
+            <button
+              className={isLastPage ? "text-gray-500" : "text-blue-500"}
+              disabled={isLastPage}
+              onClick={() => dispatchPagination({ type: "NEXT" })}
+            >
+              Next
+            </button>
+
+            <span>
+              {currentPage} / {maxPage}
+            </span>
+          </td>
+        </tr>
+      </tfoot>
     </table>
   );
 }
