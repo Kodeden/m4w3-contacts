3adc912f5c9db7e76b8abe55af3e12c1dc2a8571
Manav Misra <manavm@visionwebsoft.com>
2022-12-26 13:26:56 -0600

    refactor: :coffin: User type 🏷️

--- src/types/User.js
+++
@@ -1,9 +0,0 @@
-import PropTypes from "prop-types";
-
-export default PropTypes.exact({
-  id: PropTypes.string.isRequired,
-  avatar: PropTypes.string.isRequired,
-  fullName: PropTypes.string.isRequired,
-  phrase: PropTypes.string.isRequired,
-  username: PropTypes.string.isRequired,
-});
