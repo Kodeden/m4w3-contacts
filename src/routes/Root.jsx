eca7316a270098335093178302d0f2fcb9ee5df9
Manav Misra <manavm@visionwebsoft.com>
2022-12-26 17:58:12 -0600

    feat: :lipstick: main

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -14,29 +14,31 @@ export default function Root() {
 
   return (
     <>
-      <h1 className="mt-4 mb-12 text-center text-3xl font-bold underline">
+      <h1 className="mt-4 mb-8 text-center text-3xl font-bold underline">
         Contacts
       </h1>
-      <form className="flex flex-col items-center gap-y-4">
-        <div className="flex gap-x-4">
-          <TextInput id="fullName" pattern={/^[a-zA-Z]+$]/} />
-          <TextInput id="username" />
-          <TextInput id="phrase" />
-          <TextInput
-            id="avatar"
-            type="url"
-            placeholder="Enter URL for Avatar"
-          />
-        </div>
-        <button
-          className="w-max rounded-md bg-indigo-500 px-4 py-2 text-white hover:bg-indigo-600"
-          type="submit"
-        >
-          Submit
-        </button>
-      </form>
+      <main className="flex flex-col gap-y-4">
+        <form className="flex flex-col items-center gap-y-4 border-y py-4">
+          <div className="flex gap-x-4">
+            <TextInput id="fullName" pattern={/^[a-zA-Z]+$]/} />
+            <TextInput id="username" />
+            <TextInput id="phrase" />
+            <TextInput
+              id="avatar"
+              type="url"
+              placeholder="Enter URL for Avatar"
+            />
+          </div>
+          <button
+            className="w-max rounded-md bg-indigo-500 px-4 py-2 text-white hover:bg-indigo-600"
+            type="submit"
+          >
+            Submit
+          </button>
+        </form>
 
-      <Table headers={Object.keys(usersData[0])} data={usersData} />
+        <Table headers={Object.keys(usersData[0])} data={usersData} />
+      </main>
     </>
   );
 }
