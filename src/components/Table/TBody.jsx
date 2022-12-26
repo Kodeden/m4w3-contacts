32a895181a0b613451b9279abb5ff604abb015b1
Manav Misra <manavm@visionwebsoft.com>
2022-12-26 17:15:23 -0600

    Allow table to control usePagination

--- src/components/Table/TBody.jsx
+++ src/components/Table/TBody.jsx
@@ -1,13 +1,10 @@
-import usePagination from "@/hooks/usePagination";
 import PropTypes from "prop-types";
 import TR from "./TR";
 
 export default function TBody({ data }) {
-  const { currentData } = usePagination(data);
-
   return (
     <tbody>
-      {currentData.map((dataRow) => (
+      {data.map((dataRow) => (
         <TR key={dataRow.id} dataRow={dataRow} />
       ))}
     </tbody>
--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,12 +1,15 @@
 import PropTypes from "prop-types";
 import TBody from "./TBody";
 import TH from "./TH";
+import usePagination from "@/hooks/usePagination";
 
 export default function Table({ headers, data }) {
+  const { currentData } = usePagination(data);
+
   return (
     <table className="container mx-auto max-w-max table-auto">
       <TH headers={headers} />
-      <TBody data={data} />
+      <TBody data={currentData} />
     </table>
   );
 }
