1f365b1a83ecf35e3f58e1477d91f0b8a531fff4
Manav Misra <manavm@visionwebsoft.com>
2022-12-29 13:37:28 -0600

    feat: navigate via individual user

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,19 +1,33 @@
-import { useOutletContext, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import useUser from "../hooks/useUser";
+import { getNavIds } from "../utils/pagination";
 
 export default function User() {
-  const { data } = useOutletContext();
-  const { id } = useParams();
+  const { user, ids } = useUser();
 
-  const user = data.find((user) => user.id === id);
+  const { prevId, nextId } = getNavIds(ids, user.id);
 
   return (
-    <figure className="container mx-auto flex items-center gap-x-2 rounded-md border bg-zinc-900 px-4 py-16 text-zinc-50 shadow-xl">
-      <img src={user.avatar} alt={user.fullName} className="rounded-full" />
-      <figcaption>
-        <h2 className="text-2xl font-bold text-indigo-700">{user.fullName}</h2>
-        <small>{user.username}</small>
-        <blockquote className="italic">&quot;{user.phrase}&quot;</blockquote>
-      </figcaption>
+    <figure className="container mx-auto flex flex-col gap-y-4 rounded-md border bg-zinc-900 px-8 py-8 text-zinc-50 shadow-xl">
+      <div className="flex items-center gap-x-4">
+        <img src={user.avatar} alt={user.fullName} className="rounded-full" />
+        <figcaption>
+          <h2 className="text-2xl font-bold text-indigo-700">
+            {user.fullName}
+          </h2>
+          <small>{user.username}</small>
+          <blockquote className="italic">&quot;{user.phrase}&quot;</blockquote>
+        </figcaption>
+      </div>
+
+      <footer className="self-center text-xl">
+        <Link to={"/" + prevId} className="mr-4 text-indigo-500">
+          ⬅️ Prev
+        </Link>
+        <Link to={"/" + nextId} className="text-indigo-500">
+          Next ➡️
+        </Link>
+      </footer>
     </figure>
   );
 }
