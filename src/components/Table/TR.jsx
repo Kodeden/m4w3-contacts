9b6d5e27d8cbcdf3e8a323e98395e74b40679791
Manav Misra <manavm@visionwebsoft.com>
2022-12-29 12:35:24 -0600

    feat: :lipstick: first:text-blue-500 td

--- src/components/Table/TR.jsx
+++ src/components/Table/TR.jsx
@@ -5,7 +5,7 @@ export default function TR({ dataRow, id }) {
   return (
     <tr>
       {Object.values(dataRow).map((item, index) => (
-        <td key={index} className="truncate px-4">
+        <td key={index} className="truncate px-4 first:text-blue-500">
           <Link to={id}>
             {" "}
             {item.length > 24 ? item.substring(0, 24) + "..." : item}
