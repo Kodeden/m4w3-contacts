e875d6652c89a524291bf82f05d1738d92b11b6f
Manav Misra <manavm@visionwebsoft.com>
2022-12-29 16:29:40 -0600

    feat: :children_crossing: redirect to new contact

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,22 @@ import {
   createBrowserRouter,
   redirect,
 } from "react-router-dom";
-import Root from "./routes/Root";
-import apiService from "./services/api.service";
 import Table from "./components/Table/Table";
 import User from "./components/User";
+import Root from "./routes/Root";
+import apiService from "./services/api.service";
 
 const createUser = async ({ request }) => {
   const fd = await request.formData();
   const newUser = Object.fromEntries(fd.entries());
-  await apiService.create(newUser);
+
+  try {
+    const { id } = await apiService.create(newUser);
+    return redirect(`/${id}`);
+  } catch (error) {
+    // TODO: redirect to error page
+    console.error(error);
+  }
 };
 
 const loadUsers = async () => {
