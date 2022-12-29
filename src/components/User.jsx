d75e91325a0e2400a9c2f778531474b41434a409
Manav Misra <manavm@visionwebsoft.com>
2022-12-29 12:43:11 -0600

    feat: :lipstick: User

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -7,12 +7,12 @@ export default function User() {
   const user = data.find((user) => user.id === id);
 
   return (
-    <figure>
-      <img src={user.avatar} alt={user.fullName} />
+    <figure className="container mx-auto flex items-center gap-x-2 rounded-md border bg-zinc-900 px-4 py-16 text-zinc-50 shadow-xl">
+      <img src={user.avatar} alt={user.fullName} className="rounded-full" />
       <figcaption>
-        <h2>{user.fullName}</h2>
-        <p>{user.username}</p>
-        <p>{user.phrase}</p>
+        <h2 className="text-2xl font-bold text-indigo-700">{user.fullName}</h2>
+        <small>{user.username}</small>
+        <blockquote className="italic">&quot;{user.phrase}&quot;</blockquote>
       </figcaption>
     </figure>
   );
